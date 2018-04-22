e2dd3f41a6bdaa25a4cdaefabd35cd340bca49f0
Vijay Tiwari <vijayt@zooplus.com>
2018-04-22 10:53:08 +0200

    Initial empty repo setup

---
+++ src/main/java/org/vijayt/meetingcalendar/MeetingCalendarApplication.java
@@ -0,0 +1,12 @@
+package org.vijayt.meetingcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingCalendarApplication.class, args);
+	}
+}
